6fa20f0ed5a1458a2e8f72df7c2f85c72d387912
batmatt <m.w.1999@o2.pl>
2021-03-14 20:52:12 +0100

    New hierarchy of files

---
+++ evolutionary_algorithms/evolutionary_algorithms.py
@@ -0,0 +1,25 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+cities = []
+
+class City:
+    def __init__(self, x: int, y: int,):
+        # dictonary of distances to other cities
+        distances = {}
+
+        self.name = f"C{len(cities) + 1}"
+        self.x = x
+        self.y = y
+
+
+
+
+def main():
+    cities = 
+    cords_set = set()
+
+
+
+if __name__ == "__main__":
+    main()
