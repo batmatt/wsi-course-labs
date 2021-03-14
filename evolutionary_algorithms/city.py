de5ead320d7c3ee289167759c17acf9266548c4c
batmatt <m.w.1999@o2.pl>
2021-03-14 21:48:38 +0100

    Add City class

---
+++ evolutionary_algorithms/city.py
@@ -0,0 +1,42 @@
+import numpy as np
+
+
+class City:
+    def __init__(self, x: int, y: int, cities):
+        # dictonary of distances to other cities
+        self.distances = {}
+
+        self.name = f"C{len(cities) + 1}"
+        self.x = x
+        self.y = y
+
+        cities.append(self)
+
+    def fill_distances_dict(self, cities):
+        """
+        self, cities --> None
+
+        Fills dictonary of distances of the city
+        object basing on cities list.
+        Method checks whether we took city we are currently
+        filling distances dict - if so we put its name
+        with value 0. Else we calculate distance to other city.
+
+        """
+        for city in cities:
+            if city.name == self.name:
+                self.distances[city.name] = 0
+            else:
+                self.distances[city.name] = self.calculate_distance(city)
+
+    def calculate_distance(self, city):
+        """
+        self, city --> None
+
+        Calculates distance to other city as Euclidean
+        distance (which is identical with norm of the vector)
+
+        """
+        local_point = np.array((self.x, self.y))
+        remote_point = np.array((city.x, city.y))
+        return np.linalg.norm(local_point - remote_point)
--- evolutionary_algorithms/evolutionary_algorithms.py
+++ evolutionary_algorithms/evolutionary_algorithms.py
@@ -1,24 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from city import City
 
 cities = []
 
-class City:
-    def __init__(self, x: int, y: int,):
-        # dictonary of distances to other cities
-        distances = {}
-
-        self.name = f"C{len(cities) + 1}"
-        self.x = x
-        self.y = y
-
-
-
 
 def main():
-    cities = 
-    cords_set = set()
+    c1 = City(0, 0, cities)
+    c2 = City(5, 0, cities)
+    c3 = City(0, 5, cities)
+    c4 = City(5, 5, cities)
 
+    for city in cities:
+        city.fill_distances_dict(cities)
+    print(cities[2].distances)
 
 
 if __name__ == "__main__":
