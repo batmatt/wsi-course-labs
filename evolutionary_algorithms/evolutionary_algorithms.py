c23e2bfe2ddad63787da15271b7720284842d55d
batmatt <m.w.1999@o2.pl>
2021-03-18 16:12:59 +0100

    Finished v1.0.0 of TSP implementation

--- evolutionary_algorithms/evolutionary_algorithms.py
+++ evolutionary_algorithms/evolutionary_algorithms.py
@@ -1,5 +1,6 @@
 import argparse
 import matplotlib.pyplot as plt
+import numpy as np
 import random
 import copy
 from city import City
@@ -14,6 +15,86 @@ DISTRIBUTION_NAME = {
     "cd": "clustered",
     "rd": "random",
 }
+
+
+def evolutionary_algorithm_loop(
+    initial_population: Population,
+    population_size: int,
+    tournament_size: int,
+    mutation_threshold: float,
+    iterations_limit: int,
+    plot: bool = False,
+):
+    """
+    Function aggregating other methods into loop implementing main
+    evolutionary algorithm logic.
+
+    """
+    # setting initial best route
+    fittest_route = copy.deepcopy(initial_population.get_fittest_route())
+    print(f"Fittest route of initial population:\n{fittest_route}")
+    best_generation = 0
+
+    # if plot option is enabled store lengths of best routes for each generation
+    # in the end we plot length as a function of iteration
+    if plot:
+        fittest_routes_lengths = [fittest_route.length]
+
+    generation = 1
+    old_population = initial_population
+    while generation < iterations_limit:
+        new_population = Population()
+
+        for x in range(population_size):
+            child_route = tournament_selection(tournament_size, old_population)
+            new_population.routes_population.append(child_route)
+
+        mutate(mutation_threshold, new_population)
+
+        if new_population.get_fittest_route().length < fittest_route.length:
+            fittest_route = copy.deepcopy(new_population.get_fittest_route())
+            best_generation = generation
+
+        if plot:
+            fittest_routes_lengths.append(fittest_route.length)
+
+        # if plot option is enabled plot population's best route every 1/5 of iterations_limit
+        if plot and (
+            generation % (0.2 * iterations_limit) == 0
+            or generation == iterations_limit - 1
+        ):
+            X = []
+            Y = []
+            for city in fittest_route.sequence_of_cities:
+                X.append(city.x)
+                Y.append(city.y)
+
+            plt.plot(X[0], Y[0], marker="o", markersize=5, color="r")
+            plt.plot(X, Y, "--o")
+            plt.title(
+                f"Fittest sequence of cities in {generation} generation\nLength: {fittest_route.length}"
+            )
+
+            plt.legend(
+                [
+                    f"Starting city: {fittest_route.sequence_of_cities[0].name}",
+                ]
+            )
+            plt.show()
+
+        old_population = new_population
+        generation = generation + 1
+
+    print(
+        f"Fittest route overall:\n{fittest_route}\nFound in {best_generation}. generation"
+    )
+
+    if plot:
+        plt.plot(range(0, iterations_limit), fittest_routes_lengths)
+        plt.title("Fittest route in each generation")
+        plt.xlabel("generation")
+        plt.ylabel("length of the fittest route")
+        plt.show()
 
 
 def tournament_selection(tournament_size: int, population: Population):
@@ -107,6 +188,12 @@ def main():
     parser.add_argument(
         "-i", "--iterations", type=int, help="Maximum number of iterations"
     )
+    parser.add_argument(
+        "-plt",
+        "--plot",
+        action="store_true",
+        help="Enables plotting options",
+    )
 
     args = parser.parse_args()
 
@@ -116,17 +203,18 @@ def main():
     _tournament_size = args.tournament_size
     _mutation_threhold = args.mutation_threshold
     _iterations = args.iterations
+    _plot = args.plot
 
     _cities = []
 
-    coordinates_generator = CoordinatesGenerator(_cities_number)
+    _coordinates_generator = CoordinatesGenerator(_cities_number)
 
     if _cities_distribution == "ud":
-        points = coordinates_generator.uniform_distribution()
+        points = _coordinates_generator.uniform_distribution()
     elif _cities_distribution == "cd":
-        points = coordinates_generator.clustered_distribution()
+        points = _coordinates_generator.clustered_distribution()
     elif _cities_distribution == "rd":
-        points = coordinates_generator.random_distribution()
+        points = _coordinates_generator.random_distribution()
     else:
         print("Wrong distribution type")
         exit(1)
@@ -143,33 +231,17 @@ def main():
     )
     plt.show()
 
-    initial_population = Population(
+    _initial_population = Population(
         is_initial=True, population_size=_population_size, cities=_cities
     )
 
-    fittest_route = copy.deepcopy(initial_population.get_fittest_route())
-    print(f"Fittest route of initial population:\n{fittest_route}")
-
-    generation = 1
-    old_population = initial_population
-    while generation < _iterations:
-        new_population = Population()
-
-        for x in range(_population_size):
-            child_route = tournament_selection(_tournament_size, old_population)
-            new_population.routes_population.append(child_route)
-
-        mutate(_mutation_threhold, new_population)
-
-        if new_population.get_fittest_route().length < fittest_route.length:
-            fittest_route = copy.deepcopy(new_population.get_fittest_route())
-            best_generation = generation
-
-        old_population = new_population
-        generation = generation + 1
-
-    print(
-        f"Fittest route overall:\n{fittest_route}\nFound in {best_generation}. generation"
+    evolutionary_algorithm_loop(
+        _initial_population,
+        _population_size,
+        _tournament_size,
+        _mutation_threhold,
+        _iterations,
+        _plot,
     )
 
 
