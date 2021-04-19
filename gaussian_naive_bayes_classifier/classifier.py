1ac173fe51592f95f574a62222bcf3f19f5e374e
batmatt <m.w.1999@o2.pl>
2021-04-19 17:18:07 +0200

    Finish classifier

--- gaussian_naive_bayes_classifier/classifier.py
+++ gaussian_naive_bayes_classifier/classifier.py
@@ -1,7 +1,10 @@
 import argparse
-import numpy as np
+import math
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.model_selection import (
+    train_test_split,
+)  # i hope it's not cheating if i use this method just to split dataset neatly
 from pandas.core.frame import DataFrame
 
 X1 = "sepal length [cm]"
@@ -21,12 +24,109 @@ def load_data_from_file(filename: str) -> DataFrame:
     return df
 
 
-def calculate_statistics(dataset: DataFrame):
-    dataset_mean = dataset[[X1, X2, X3, X4]].mean()
+def calculate_statistics_by_class(train_set: DataFrame):
+    groups = train_set.groupby(CLASS)
 
-    dataset_std_deviation = dataset[[X1, X2, X3, X4]].std()
+    mean_values_by_class = {}
+    std_deviation_values_by_class = {}
+    prior_probability_by_class = {}
 
-    return dataset_mean, dataset_std_deviation
+    for class_name, group in groups:
+        mean_values_by_class[class_name] = group[[X1, X2, X3, X4]].mean()
+        std_deviation_values_by_class[class_name] = group[[X1, X2, X3, X4]].std()
+        prior_probability_by_class[class_name] = len(group) / len(train_set)
+
+    return (
+        mean_values_by_class,
+        std_deviation_values_by_class,
+        prior_probability_by_class,
+    )
+
+
+def gaussian_density_function(x: float, mean: float, std_deviation: float) -> float:
+    exponent = math.exp(-((x - mean) ** 2 / (2 * std_deviation ** 2)))
+
+    return exponent / (std_deviation * math.sqrt(2 * math.pi))
+
+
+def calculate_posterior_probabilities(
+    feature_vector: list,
+    mean_values: list,
+    std_deviation_values: list,
+    prior_probabilities: list,
+) -> dict:
+    posterior_probabilities = {}
+    features_number = len(feature_vector)
+
+    for class_name in mean_values.keys():
+        likelihood = 1
+
+        for index in range(features_number):
+            feature = feature_vector[index]
+            mean = mean_values[class_name][index]
+            std_deviation = std_deviation_values[class_name][index]
+
+            gaussian_probability = gaussian_density_function(
+                feature, mean, std_deviation
+            )
+            likelihood = likelihood * gaussian_probability
+
+        prior_probability = prior_probabilities[class_name]
+        joint_probability = prior_probability * likelihood
+        posterior_probabilities[class_name] = joint_probability
+
+    return posterior_probabilities
+
+
+def make_prediction(
+    feature_vector: list,
+    mean_values: list,
+    std_deviation_values: list,
+    prior_probabilities: list,
+) -> str:
+    posterior_probabilities = calculate_posterior_probabilities(
+        feature_vector, mean_values, std_deviation_values, prior_probabilities
+    )
+    prediction = max(posterior_probabilities, key=posterior_probabilities.get)
+
+    return prediction
+
+
+def test_set_classification(
+    test_set: DataFrame,
+    mean_values: list,
+    std_deviation_values: list,
+    prior_probabilities: list,
+):
+    results = pd.DataFrame(columns=["prediction", "label", "result"])
+
+    for index, row in test_set.iterrows():
+        feature_vector = row.to_list()
+
+        # extract label
+        label = feature_vector[-1]
+        # remove label from feature vector
+        feature_vector.pop()
+
+        prediction = make_prediction(
+            feature_vector,
+            mean_values,
+            std_deviation_values,
+            prior_probabilities,
+        )
+
+        if prediction == label:
+            results = results.append(
+                {"prediction": prediction, "label": label, "result": 1},
+                ignore_index=True,
+            )
+        else:
+            results = results.append(
+                {"prediction": prediction, "label": label, "result": 0},
+                ignore_index=True,
+            )
+
+    return results
 
 
 def plot_labels_by_two_features(
@@ -50,6 +150,17 @@ def plot_labels_by_two_features(
     plt.clf()
 
 
+def prepare_train_and_test_set(
+    dataset: DataFrame, train_size: float, is_train_set_sorted: bool = False
+):
+    train_set, test_set = train_test_split(dataset, train_size=train_size)
+
+    if is_train_set_sorted:
+        train_set = train_set.sort_index()
+
+    return train_set, test_set
+
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument(
@@ -61,21 +172,44 @@ def main():
 
     parser.add_argument(
         "-ts",
-        "--training-size",
+        "--train-size",
         type=float,
-        help="Percentage of dataset used in the training set.",
+        help="Percentage of dataset used in the train set.",
+    )
+
+    parser.add_argument(
+        "-srt",
+        "--sort-train-set",
+        action="store_true",
+        help="Sorts train set after spliting dataset to train and test set",
     )
 
     args = parser.parse_args()
 
     _filename = args.filename
-    _training_size = args.training_size
+    _train_size = args.train_size
+    _sort_train_set = args.sort_train_set
 
     dataset = load_data_from_file(_filename)
-    statistics = calculate_statistics(dataset)
 
-    # uncomment to check grouping by two chosen features
-    plot_labels_by_two_features(X1, X2, dataset)
+    # uncomment to check grouping classes by two chosen features
+    # plot_labels_by_two_features(X1, X2, dataset)
+
+    train_set, test_set = prepare_train_and_test_set(
+        dataset, _train_size, _sort_train_set
+    )
+
+    (
+        mean_values,
+        std_variation_values,
+        prior_probabilities,
+    ) = calculate_statistics_by_class(train_set)
+
+    results = test_set_classification(
+        test_set, mean_values, std_variation_values, prior_probabilities
+    )
+
+    print(results)
 
 
 if __name__ == "__main__":
