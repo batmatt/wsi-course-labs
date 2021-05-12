e4b0dcb9a53b818ec3c6b3c30db99b008e21b5ed
batmatt <m.w.1999@o2.pl>
2021-05-12 13:38:29 +0200

    Splitting dataset by gini index

--- random_forest/classifier.py
+++ random_forest/classifier.py
@@ -7,10 +7,11 @@ Dataset source: https://archive.ics.uci.edu/ml/machine-learning-databases/car/
 """
 
 import argparse
-import math
+from typing import List
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+from scipy.sparse import data
 import seaborn as sn
 from sklearn.model_selection import (
     train_test_split,
@@ -40,26 +41,95 @@ def load_data_from_file(filename: str) -> DataFrame:
     return df
 
 
-def prepare_train_and_test_set(
-    dataset: DataFrame, train_size: float, is_train_set_sorted: bool = False
-):
-    train_set, test_set = train_test_split(dataset, train_size=train_size)
-
-    if is_train_set_sorted:
-        train_set = train_set.sort_index()
-
-    return train_set, test_set
-
-
-def get_test_set_classes(test_set: DataFrame):
+def get_dataset_classes(dataset: DataFrame):
     classes = {}
     i = 0
-    for index in test_set.index:
-        if test_set[CLASS][index] not in classes.values():
-            classes[i] = test_set[CLASS][index]
+    for index in dataset.index:
+        if dataset[CLASS][index] not in classes.values():
+            classes[i] = dataset[CLASS][index]
             i = i + 1
 
     return classes
+
+
+def evaluate_wiht_cross_validation(train_set: DataFrame, folds_number: int):
+    split = np.array_split(train_set, folds_number)
+
+    for i in range(folds_number):
+        train_set_parts = split.copy()
+        del train_set_parts[i]
+
+        train_set = pd.concat(train_set_parts, sort=False)
+        test_set = split[i]
+
+
+def test_split(dataset: DataFrame, feature: str, value: str):
+    left = dataset[dataset[feature] == value]
+    right = dataset[dataset[feature] != value]
+
+    return left, right
+
+
+def calculate_gini_index(left: DataFrame, right: DataFrame, class_values: list):
+    gini_index = 0
+    branches = [left, right]
+
+    for class_value in class_values:
+        for branch in branches:
+            branch_size = len(branch.index)
+            if branch_size == 0:
+                continue
+
+            class_value_occurances = branch.loc[
+                branch[CLASS] == class_value, CLASS
+            ].count()
+            proportion = class_value_occurances / branch_size
+            gini_index += proportion * (1 - proportion)
+
+    return gini_index
+
+
+def get_best_split(dataset: DataFrame):
+    class_values = dataset[CLASS].tolist()
+    previous_best_gini_index = 10000
+    best_feature = ""
+    best_value = ""
+    best_left = ""
+    best_right = ""
+
+    for feature in FEATURES:
+        values = list(dict.fromkeys(dataset[feature].tolist()))
+
+        for value in values:
+            left, right = test_split(dataset, feature, value)
+            gini_index = calculate_gini_index(left, right, class_values)
+            print(f"gini: {gini_index}, feature: {feature}, value: {value}")
+
+            if gini_index < previous_best_gini_index:
+                previous_best_gini_index = gini_index
+                best_feature = feature
+                best_value = value
+                best_left = left
+                best_right = right
+
+    best_split = {
+        "feature": best_feature,
+        "value": best_value,
+        "left": best_left,
+        "right": best_right,
+    }
+    return best_split
+
+
+def split_on_node(node: dict, max_depth: int, current_depth: int):
+    left, right = node["left"], node["right"]
+    del node["left"]
+    del node["right"]
+    pass
+
+
+def build_decision_tree(train_set: DataFrame, max_depth: int):
+    tree_root = get_best_split(train_set)
 
 
 def plot_features_histograms(dataset: DataFrame):
@@ -142,17 +212,17 @@ def plot_features_histograms(dataset: DataFrame):
                         feature_values, all_feature_values, occurrences
                     )
 
-                if key == "acc":
+                if key == "vgood":
                     vgood_occurrences = create_occurrences_lists(
                         feature_values, all_feature_values, occurrences
                     )
 
         df = pd.DataFrame(
             {
-                "unacc_occurrences": unacc_occurrences,
-                "acc_occurrences": acc_occurrences,
-                "good_occurrences": good_occurrences,
-                "vgood_occurrences": vgood_occurrences,
+                "unacc": unacc_occurrences,
+                "acc": acc_occurrences,
+                "good": good_occurrences,
+                "vgood": vgood_occurrences,
             },
             index=all_feature_values,
         )
@@ -188,6 +258,34 @@ def main():
     )
 
     parser.add_argument(
+        "-ts",
+        "--train-size",
+        type=float,
+        help="Percentage of dataset used in the train set.",
+    )
+
+    parser.add_argument(
+        "-fn",
+        "--folds_number",
+        type=int,
+        help="Number of folds used in cross-validation",
+    )
+
+    parser.add_argument(
+        "-md",
+        "--max-depth",
+        type=int,
+        help="Maximum depth of decision trees",
+    )
+
+    parser.add_argument(
+        "-tn",
+        "--trees-number",
+        type=int,
+        help="Number of trees used in random forest",
+    )
+
+    parser.add_argument(
         "-hist",
         "--plot-histograms",
         action="store_true",
@@ -197,12 +295,23 @@ def main():
     args = parser.parse_args()
 
     _filename = args.filename
+    _train_size = args.train_size
+    _folds_number = args.folds_number
+    _max_depth = args.max_depth
+    _trees_number = args.trees_number
     _plot_histograms = args.plot_histograms
 
     dataset = load_data_from_file(_filename)
 
     if _plot_histograms:
         plot_features_histograms(dataset)
+
+    train_set, validation_set = train_test_split(dataset, train_size=_train_size)
+
+    evaluate_wiht_cross_validation(train_set, _folds_number)
+
+    best_split = get_best_split(train_set)
+    print(best_split)
 
 
 if __name__ == "__main__":
